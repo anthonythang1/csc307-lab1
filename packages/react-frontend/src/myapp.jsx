83609a5351bbfe9bdbbc94a53b2d22493b43b979
anthonythang1 <anthonythang64@Yahoo.com>
2024-12-04 22:33:30 -0800

    post call added to frontend

--- packages/react-frontend/src/myapp.jsx
+++ packages/react-frontend/src/myapp.jsx
@@ -1,25 +1,58 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Table from "./Table";
 import Form from "./Form";
 
 function MyApp() {
-  const [characters, setCharacters] = useState([]);
+    const [characters, setCharacters] = useState([]);
 
-  function removeOneCharacter(index) {
-    const updated = characters.filter((character, i) => i !== index);
-    setCharacters(updated);
-  }
 
-  function updateList(person) {
-    setCharacters([...characters, person]); // Add new character
-  }
+    function fetchUsers() {
+        const promise = fetch("http://localhost:8000/users");
+        return promise;
+    }
 
-  return (
-    <div className="container">
-      <Table characterData={characters} removeCharacter={removeOneCharacter} />
-      <Form handleSubmit={updateList} />
-    </div>
-  );
+    function postUser(person) {
+        const promise = fetch("http://localhost:8000/users", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(person),
+        });
+
+        return promise;
+    }
+
+    useEffect(() => {
+        fetchUsers()
+            .then((res) => res.json())
+            .then((json) => setCharacters(json["users_list"]))
+            .catch((error) => {
+                console.error("Error fetching users:", error);
+            });
+    }, []); 
+
+  
+    function updateList(person) {
+        postUser(person)
+            .then(() => setCharacters([...characters, person]))
+            .catch((error) => {
+                console.error("Error adding user:", error);
+            });
+    }
+
+  
+    function removeOneCharacter(index) {
+        const updated = characters.filter((character, i) => i !== index);
+        setCharacters(updated);
+    }
+
+    return (
+        <div className="container">
+            <Table characterData={characters} removeCharacter={removeOneCharacter} />
+            <Form handleSubmit={updateList} />
+        </div>
+    );
 }
 
 export default MyApp;
