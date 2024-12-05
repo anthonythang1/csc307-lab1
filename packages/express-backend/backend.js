2cddc7c1e7422972d8f5aa156ec8dcb9dad7a041
anthonythang1 <anthonythang64@Yahoo.com>
2024-12-04 21:49:01 -0800

    delete by id

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -16,6 +16,15 @@ const addUser = (user) => {
   return user;
 };
 
+const deleteUserById = (id) => {
+  const index = users["users_list"].findIndex((user) => user.id === id);
+  if (index !== -1) {
+    users["users_list"].splice(index, 1);
+    return true;
+  }
+  return false;
+};
+
 app.use(express.json());
 
 app.get("/", (req, res) => {
@@ -47,6 +56,16 @@ app.post("/users", (req, res) => {
   const userToAdd = req.body;
   addUser(userToAdd);
   res.send();
+});
+
+app.delete("/users/:id", (req, res) => {
+  const id = req.params.id;
+  const success = deleteUserById(id);
+  if (success) {
+    res.status(204).send(); // No content, success
+  } else {
+    res.status(404).send("Resource not found.");
+  }
 });
 
 app.listen(port, () => {
